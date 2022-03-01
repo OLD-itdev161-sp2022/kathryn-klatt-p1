c1907c420433e9375b9c47e79c874ebf05859cf5
Lint404 <klattkl@gmatc.matc.edu>
2022-02-28 19:14:27 -0600

    Posts are not saving to DB...

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/Post');
+const Post = require("../models/Post");
 
 //ROUTES
 router.get("/", (req, res) => {
   res.send("We are home.");
 });
 
-router.post('/', (req,res)=> {
-    console.log(req.body);
+router.post("/", (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  post
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
 });
 
-module.exports=router;
+module.exports = router;
